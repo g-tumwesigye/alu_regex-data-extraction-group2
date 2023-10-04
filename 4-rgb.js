313922700b64197c30737be65ae0fb355d7fedef
Euwamahoro <133766701+Euwamahoro@users.noreply.github.com>
2023-10-04 14:02:14 +0200

    Update 4-rgb.js

--- 4-rgb.js
+++ 4-rgb.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
+const data = "RGB Color: (255, 255, 255)";
+const rgb_pattern = /RGB Color: \((\d+), (\d+), (\d+)\)/;
 const rgb_match = data.match(rgb_pattern);
-if (rgb_match
-const [r, g, b] = rgb_match.slice(1);
-console.log(RGB Color: (${r}, ${g}, ${b}));
+
+if (rgb_match) {
+  const [r, g, b] = rgb_match.slice(1);
+  console.log(`RGB Color: (${r}, ${g}, ${b})`);
 }
