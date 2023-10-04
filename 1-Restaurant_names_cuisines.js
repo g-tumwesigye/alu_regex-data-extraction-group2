a4820a62dbaff3c045efcf242289e1f1f613d5ca
Euwamahoro <133766701+Euwamahoro@users.noreply.github.com>
2023-10-04 20:38:20 +0200

    This is Enock

--- 1-Restaurant_names_cuisines.js
+++ 1-Restaurant_names_cuisines.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
-const text = "Restaurant Name - Cuisine";
+
+const apiResponses = [/* input the API responses here */];
 const regex = /(.+) - (.+)/;
-const match = text.match(regex);
-if (match) {
-  const restaurantName = match[1];
-  const cuisine = match[2];
-  console.log("Restaurant Name:", restaurantName);
-  console.log("Cuisine:", cuisine);
-} else {
-  console.log("Ooops, let keep searching")
-}
+
+// Process each API response
+apiResponses.forEach((text, index) => {
+  const match = text.match(regex);
+
+  if (match) {
+    const restaurantName = match[1];
+    const cuisine = match[2];
+    console.log(`API Response ${index + 1}:`);
+    console.log('Restaurant Name:', restaurantName);
+    console.log('Cuisine:', cuisine);
+  } else {
+    console.log(`API Response ${index + 1}:`);
+    console.log("Oops, let's keep searching");
+  }
+
+  console.log('\n'); // Add a line break for clarity
+});
